ce377a00714df20178ad24c24204eaf439b08191
Andrew Aquino <DrewKiino@Atlantis.local>
2015-10-18 17:14:44 -0700

    probably broke this branch

---
+++ .tmp/public/js/tutorial1.js
@@ -0,0 +1,27 @@
+"user strict"
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <CommentBox />,
+  // <CommentForm />,
+  document.getElementById('content')
+);
---
+++ assets/js/helloworld.js
@@ -0,0 +1,22 @@
+var Counter = React.createClass({
+  incrementCount: function(){
+    this.setState({
+      count: this.state.count + 1
+    });
+  },
+  getInitialState: function(){
+     return {
+       count: 0
+     }
+  },
+  render: function(){
+    return (
+      <div className="counter">
+        <h1>{this.state.count}</h1>
+        <button type="button" onClick={this.incrementCount}>Increment</button>
+      </div>
+    );
+  }
+});
+
+React.renderComponent(React.createElement(Counter, null), document.getElementById('mount-point'));
