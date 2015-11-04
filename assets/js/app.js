9f9a4627c86c5c50796b05bb18da9cb14d7a4b9e
DrewKiino <andrewaquino118@yahoo.com>
2015-11-03 16:56:01 -0800

    converting songs to navy web app

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+/**
+ * the browserify framework allows us to import Node.js dependencies
+ * using the 'require' method
+ */
+
+window.LiveReloadOptions = { host: 'localhost' }
+require('livereload-js')
+
+var Promise = require('bluebird')
