22efc0a88c09b6f14a883731343d1354498a7588
DrewKiino <andrewaquino118@yahoo.com>
2015-11-05 20:03:38 -0800

    created apps list

--- api/controllers/AppsController.js
+++ api/controllers/AppsController.js
@@ -12,9 +12,21 @@ export function getApps(req, res) {
 
   return res.json({
     items: [
-      'bob',
-      'harry',
-      'amy'
+      {
+        name: 'missle targetter',
+        author: 'some fool',
+        description: 'a cool app that lets you call in air strikes'
+      },
+      {
+        name: 'navy social app!',
+        author: 'some fool 2',
+        description: 'an app for navy buddies to get connected together with'
+      },
+      {
+        name: 'mine bomb tracker',
+        author: 'some fool 3',
+        description: 'an app that acts as a mobile mine sweeper, you will get a notification whenever your within 10 feet of a dangerous mine.'
+      }
     ]
   })
 }
--- api/models/Apps.js
+++ api/models/Apps.js
@@ -5,10 +5,28 @@
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
 
-module.exports = {
 
-  attributes: {
+import mongoose from 'mongoose'
 
-  }
-};
+
+const schema = new mongoose.Schema({
+
+  author: { type: String },
+
+  name: { type: String },
+
+  description: { type: String },
+
+  downloads: { type: String },
+
+  rating: { type: String },
+
+  price: { type: String },
+
+  image: { type: String },
+
+  downloadUrl: { type: String }
+})
+
+exports.schema = schema
 
