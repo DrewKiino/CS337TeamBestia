0144ddd03f4d739079b17ee1b74bbba79281570c
DrewKiino <andrewaquino118@yahoo.com>
2015-11-08 00:23:40 -0800

    added bson module and added upload form in index.html

--- api/controllers/AppsController.js
+++ api/controllers/AppsController.js
@@ -14,9 +14,31 @@ export function getApps(req, res) {
   return Apps.mongoose.findAsync()
   .then( results => {
     sails.log.info(results.length)
-    return res.json(results)
+    return res.json(results + ' results')
   })
   .catch( error => {
     return res.error(error)
   })
+}
+
+export function submit(req, res) {
+  sails.log('attempting to save submitted app')
+
+  req.file('file').upload({
+    adapter: require('skipper-gridfs'),
+    uri: 'mongodb://localhost:27017/sails'
+  }, function(err, files) {
+    sails.log.error(err)
+    sails.log(files)
+  })
+
+
+  // const {
+  //   name,
+  //   author,
+  //   price,
+  //   description
+  // } = req.body
+
+  // console.log(req.body)
 }
